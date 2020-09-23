790f2551f55453eda711effd174e81248169e5c6
amnaweed <71783728+amnaweed@users.noreply.github.com>
2020-09-23 22:05:35 +0430

    Update code.py

--- code.py
+++ code.py
@@ -10,3 +10,11 @@ val=input("Write text here: ")
 
 translation = translator.translate(val)
 print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+
+#The below code can swap two  variables
+x=10
+y=90
+
+c = x
+x = y
+y = c
