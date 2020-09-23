1ff29d9c62d13b7aea91da0eb9e0d408bf80e1cf
amnaweed <71783728+amnaweed@users.noreply.github.com>
2020-09-23 21:56:01 +0430

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,9 @@
+#This is a translator app that will translate any given text in dari and pashtu to English
 import googletrans
 from googletrans import Translator, constants
 from pprint import pprint
 
+#init the Google API translator
 translator = Translator()
 
 val=input("Write text here: ")
